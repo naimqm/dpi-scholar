9d4f16601d26535b6b4fcc8850d66c106227041d
naimqm <57574856+naimqm@users.noreply.github.com>
2021-08-25 11:18:46 -0500

    Completion of Backend and Beginning of API

--- django-todo-react/backend/todo/models.py
+++ django-todo-react/backend/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length = 120) # Field for title of todo-item
+    description = models.TextField() # Text field for description of todo-item
+    completed = models.BooleanField(default = False) # Status of completion for the todo-item, defaulting to false
+
+    def _str_(self):
+        return self.title
